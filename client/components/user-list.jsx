b3a11abd06ffc0ee53a47aa8d9ab96b2bc4b44ba
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-05-30 21:47:47 +0000

    Creating dummy data

---
+++ client/components/user-list.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default class userList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    fetch()
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          {
+            this.state.users.map(user => (
+              <div key={user.userId} className='col-12 col-md-6 col-lg-4'>
+                <User user={user} />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+function User(props) {
+  const { userId, name, imageUrl, rank, role, championId } = props.user;
+  return (
+    <a
+      href={`#users?userId=${userId}`}
+      className='text-dark card mb-4 shadow-sm text-decoration-none'>
+      <img src={imageUrl} className='card-img-top' alt={name} />
+      <div className='card-body'>
+        <h5 className='card-title'> { name } </h5>
+        <p>Rank: { rank }</p>
+        <p>Roles: { role }</p>
+        <p>Champions: { championId }</p>
+      </div>
+    </a>
+  );
+}
---
+++ database/data.sql
@@ -0,0 +1,14 @@
+insert into "users" (
+  "userId",
+  "username",
+  "hashedPassword",
+  "createdAt",
+  "imageUrl",
+  "name",
+  "bio",
+  "profile"
+) values (
+  1,
+  'Soren',
+  'password',
+)
