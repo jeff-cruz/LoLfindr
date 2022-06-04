8daaa47086ada6d9e204a3ec24a4730f3d242e2f
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-06-04 23:02:13 +0000

    Testing out getting user card data by searching by roleId

--- server/index.js
+++ server/index.js
@@ -96,12 +96,12 @@ app.get('/api/filter', (req, res, next) => {
   // const roleId = req.query.roleId;
   // const championId = req.query.championId;
 
-  if (!rankId) {
-    res.status(400).json({
-      error: 'Valid rankId (string) are required fields'
-    });
-    return;
-  }
+  // if (!rankId) {
+  //   res.status(400).json({
+  //     error: 'Valid rankId (string) are required fields'
+  //   });
+  //   return;
+  // }
 
   const sql = `
     select "u"."userId",
@@ -134,6 +134,48 @@ app.get('/api/filter', (req, res, next) => {
     `;
 
   const query = [rankId];
+  db.query(sql, query)
+    .then(result => {
+      res.json(result.rows);
+    })
+    .catch(err => next(err));
+});
+
+// get user card data by searching by role
+app.get('/api/role', (req, res, next) => {
+  const roleId = req.query.roleId;
+
+  const sql = `
+    select "u"."userId",
+            "u"."name",
+            "u"."imageUrl",
+            "c"."champions",
+            "rl".*,
+            "rk".*
+      from "users" as "u"
+    join "ranks" as "rk" using ("rankId")
+    left join lateral (
+      select json_agg("c") as "champions"
+      from (
+        select "c".*
+        from "champions" as "c"
+        join "userChampions" as "uc" using ("championId")
+        where "uc"."userId" = "u"."userId"
+      ) as "c"
+    ) as "c" on true
+    left join lateral (
+      select json_agg("rl") as "roles"
+      from (
+        select "rl".*
+        from "roles" as "rl"
+        join "userRoles" as "url" using ("roleId")
+        where "url"."userId" = "u"."userId"
+      ) as "rl"
+    ) as "rl" on true
+    where "roles"."roleId" = $1
+    `;
+
+  const query = [roleId];
   db.query(sql, query)
     .then(result => {
       res.json(result.rows);
