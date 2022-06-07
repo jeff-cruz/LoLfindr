f8ee5d7c17b22c2e30df1463c72600d2838b76ff
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-06-07 20:13:06 +0000

    User can sign into their account using username and password

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import AppContext from '../lib/app-context';
 
-export default function Header(props) {
-  return (
-    <header className='navbar navbar-expand-lg fixed-top'>
-      <a href=''>
-      <img className="logo" src="images/icon.png" alt="LoLFindr Icon" />
-      </a>
-    </header>
-  );
+export default class Header extends React.Component {
+  render() {
+    // const { handleSignOut } = this.context;
+
+    return (
+
+      <header className='navbar navbar-expand-lg fixed-top'>
+        <a href=''>
+        <img className="logo" src="images/icon.png" alt="LoLFindr Icon" />
+        </a>
+        {/* <button className='sign-out' onClick={handleSignOut}>Sign Out</button> */}
+      </header>
+    );
+  }
 }
+
+Header.contextType = AppContext;
