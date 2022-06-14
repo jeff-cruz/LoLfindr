47b64978f6033c7e095925f08403cd5750e729b8
Jeffrey Cruz <jeff.jc.cruz@gmail.com>
2022-06-14 20:15:34 +0000

    In progress

--- server/uploads-middleware.js
+++ server/uploads-middleware.js
@@ -1,19 +1,48 @@
 const path = require('path');
+const mime = require('mime');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const S3 = require('aws-sdk/clients/s3');
 
-const imagesDirectory = path.join(__dirname, 'public/images');
+const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, imagesDirectory);
-  },
-  filename: (req, file, callback) => {
+const storage = multerS3({
+  s3,
+  bucket: process.env.AWS_S3_BUCKET,
+  acl: 'public-read', // so that users can view the files
+  key: (req, file, done) => {
     const fileExtension = path.extname(file.originalname);
-    const name = `${file.fieldname}-${Date.now()}${fileExtension}`;
-    callback(null, name);
+    // include the extension in the key (.jpg, .png, .wav, .mp4, etc)
+    const key = `${Date.now()}${fileExtension}`;
+    done(null, key);
+  },
+  contentType: (req, file, done) => {
+    // tell S3 what Content-Type to use when serving the file
+    const contentType = mime.getType(file.originalname);
+    done(null, contentType);
   }
 });
 
-const uploadsMiddleware = multer({ storage }).single('image');
+const uploadsMiddleware = multer({
+  storage
+}).single('file-to-upload');
 
 module.exports = uploadsMiddleware;
+
+// const imagesDirectory = path.join(__dirname, 'public/images');
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, imagesDirectory);
+//   },
+//   filename: (req, file, callback) => {
+//     const fileExtension = path.extname(file.originalname);
+//     const name = `${file.fieldname}-${Date.now()}${fileExtension}`;
+//     callback(null, name);
+//   }
+// });
+
+// const uploadsMiddleware = multer({ storage }).single('image');
